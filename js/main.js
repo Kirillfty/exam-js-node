03dead5d7e18b371be1d933ab8002c86ace34c20
Kirillfty <kirillhaiduk4@gmail.com>
2024-05-25 23:03:08 +0300

    gotovo
    yahaha

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-const Get_U = async function (){
-    await fetch("json/users.json").then(function(responce){
-        return responce.json();
-    }).then(function(dataUser){
-        console.log(dataUser);
-    })
-}
-Get_U();
+
 let inp = document.querySelector('#inp');
 let inp1 = document.querySelector('#inp1');
 let inp2 = document.querySelector('#inp2');
@@ -18,23 +11,10 @@ let user = {
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
-    Post();
-})
-const Post = async function (user){
-     user = {
+    user = {
         email:inp2.value,
         name:inp.value,
         password:inp1.value
     }
-    await fetch("json/users.json/", {
-        method: "PUT",
-        body: JSON.stringify({
-          email: inp2.value,
-          name: inp.value,
-          password: inp1.value
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8"
-        }
-      });
-}
+    localStorage.setItem('user',JSON.stringify(user));
+})
